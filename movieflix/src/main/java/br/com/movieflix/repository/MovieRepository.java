0c8dc0fe30518212c601605267f3af320202f567
Lucas Sabadini Mendes <lucas.mendes@alpha.corp>
2026-09-01 17:00:57 -0300

    feat: adicionando update e delete de funcionalidade no projeto

--- movieflix/src/main/java/br/com/movieflix/repository/MovieRepository.java
+++ movieflix/src/main/java/br/com/movieflix/repository/MovieRepository.java
@@ -1,9 +1,16 @@
 package br.com.movieflix.repository;
 
+import br.com.movieflix.entity.Category;
 import br.com.movieflix.entity.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface MovieRepository extends JpaRepository<Movie, Long> {
+
+    List<Movie> findByCategoriesId(Long categoryId);
+
+    List<Movie> findTop5ByOrderByRatingDesc();
 }
--- movieflix/src/main/java/br/com/movieflix/service/MovieService.java
+++ movieflix/src/main/java/br/com/movieflix/service/MovieService.java
@@ -29,8 +29,42 @@ public class MovieService {
         return movieRepository.findAll();
     }
 
+    public List<Movie> findByCategory(Long categoryId) {
+        return movieRepository.findByCategoriesId(categoryId);
+    }
+
     public Optional<Movie> findById(Long id) {
         return movieRepository.findById(id);
+    }
+
+    public Optional<Movie> update(Long movieId, Movie updateMovie) {
+        Optional<Movie> optMovie = movieRepository.findById(movieId);
+        if (optMovie.isPresent()) {
+
+            List<Category> categories = this.findCategories(updateMovie.getCategories());
+            List<Streaming> streamings = this.findStreamings(updateMovie.getStreamings());
+
+            Movie movie = optMovie.get();
+            movie.setTitle(updateMovie.getTitle());
+            movie.setDescription(updateMovie.getDescription());
+            movie.setReleaseDate(updateMovie.getReleaseDate());
+            movie.setRating(updateMovie.getRating());
+
+            movie.getCategories().clear();
+            movie.getCategories().addAll(categories);
+
+            movie.getStreamings().clear();
+            movie.getStreamings().addAll(streamings);
+
+            movieRepository.save(movie);
+            return Optional.of(movie);
+        }
+
+        return Optional.empty();
+    }
+
+    public void deleteMovie(Long id) {
+        movieRepository.deleteById(id);
     }
 
     private List<Category> findCategories(List<Category> categories) {
