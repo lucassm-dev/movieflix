b3fe3f898570fcfb10cbff0d2d4b5237733e1821
Lucas Sabadini Mendes <lucas.mendes@alpha.corp>
2026-09-02 15:42:58 -0300

    fix: corrige import errado

--- movieflix/src/main/java/br/com/movieflix/repository/MovieRepository.java
+++ movieflix/src/main/java/br/com/movieflix/repository/MovieRepository.java
@@ -1,6 +1,5 @@
 package br.com.movieflix.repository;
 
-import br.com.movieflix.entity.Category;
 import br.com.movieflix.entity.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
