f535f9f9443999c37c1e7ff6001753a537f30e78
Lucas Mendes <lucassabadinimendes@gmail.com>
2026-09-03 06:57:11 -0300

    refactor: altera UserRepository por causa do login/cadastro - spring security

--- movieflix/src/main/java/br/com/movieflix/repository/UserRepository.java
+++ movieflix/src/main/java/br/com/movieflix/repository/UserRepository.java
@@ -2,8 +2,13 @@ package br.com.movieflix.repository;
 
 import br.com.movieflix.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<UserDetails> findUserByEmail(String email);
 }
